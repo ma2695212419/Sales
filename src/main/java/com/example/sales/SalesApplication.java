94ebd8f98bfd93872a2cc00b34bf03991405195d
马泽朋 <2695212419@qq.com>
2020-03-03 18:00:44 +0800

    添加作者信息

--- src/main/java/com/example/sales/SalesApplication.java
+++ src/main/java/com/example/sales/SalesApplication.java
@@ -3,6 +3,9 @@ package com.example.sales;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Administrator
+ */
 @SpringBootApplication
 public class SalesApplication {
 
